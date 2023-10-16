85fe8ae46969687a7e0f894de0c43af133b2ee1f
Riyaz1203 <mohammedriyaz12032003@gmail.com>
2023-10-16 12:17:11 +0530

    welcome page

---
+++ lib/pages/welcome.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class welcomepage extends StatelessWidget {
+  const welcomepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Column(
+      children: [],
+    ));
+  }
+}
